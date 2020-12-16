cf9098ddf93a4de7d6db5df98a4744088b24cda4
laterinthepast <laterinthepast.0@gmail.com>
2020-12-16 20:46:05 +0000

    changed message at front

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,13 +3,37 @@ import React from 'react';
 import './HeroSection.css';
 import '../App.css';
 
+
+
+
+
+let hello = [
+    "hello",
+    "cześć",
+    "hallo",
+    "ciao",
+    "olá",
+    "你好"
+];
+let i =0;
+
+setInterval (function (){
+    document.getElementById("random").innerHTML = hello[i];
+    if (i=== (hello.length-1)){
+        i=0;
+    }else {
+        i++;
+    }
+},1000);
+
+
 function HeroSection() {
     return (
         <div className='hero-container' id="welcome">
             {/* <video src="/video/welcome.mp4" autoPlay loop muted></video>  */}
-                <h1>zero</h1>
-                <p>web development</p>
-                
+            <h1><span id="random"></span></h1>
+            <p>this.isMyPortfolio</p>
+
 
         </div>
     )
